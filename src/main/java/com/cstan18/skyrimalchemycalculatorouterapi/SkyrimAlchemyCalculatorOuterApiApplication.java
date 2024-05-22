698709680885d8820dc925c05ff8679ff314f337
Caleb Standridge <caleb.standridge@csuglobal.edu>
2024-05-22 11:10:12 -0500

    init commit

---
+++ src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/SkyrimAlchemyCalculatorOuterApiApplication.java
@@ -0,0 +1,13 @@
+package com.cstan18.skyrimalchemycalculatorouterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyrimAlchemyCalculatorOuterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyrimAlchemyCalculatorOuterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cstan18/skyrimalchemycalculatorouterapi/SkyrimAlchemyCalculatorOuterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cstan18.skyrimalchemycalculatorouterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyrimAlchemyCalculatorOuterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
