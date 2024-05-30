b16c1702c4ce1ae0193bbe382d4b96bb630fd248
Caleb Standridge <caleb.standridge@csuglobal.edu>
2024-05-30 09:05:19 -0500

    optimize class structure

--- src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/controller/AlchemyController.java
+++ src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/controller/AlchemyController.java
@@ -2,9 +2,6 @@ package com.cstan18.skyrimalchemycalculatorouterapi.controller;
 
 import com.cstan18.skyrimalchemycalculatorouterapi.dto.request.IngredientQuantityRequest;
 import com.cstan18.skyrimalchemycalculatorouterapi.dto.response.IngredientQuantityResponse;
-import com.cstan18.skyrimalchemycalculatorouterapi.model.Effect;
-import com.cstan18.skyrimalchemycalculatorouterapi.model.Ingredient;
-import com.cstan18.skyrimalchemycalculatorouterapi.model.PotionRecipe;
 import com.cstan18.skyrimalchemycalculatorouterapi.service.PotionRecipeService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
--- src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/dto/request/IngredientQuantityRequest.java
+++ src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/dto/request/IngredientQuantityRequest.java
@@ -1,18 +1,14 @@
 package com.cstan18.skyrimalchemycalculatorouterapi.dto.request;
 
 import com.cstan18.skyrimalchemycalculatorouterapi.dto.IngredientQuantityDTO;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@Data
+@RequiredArgsConstructor
 public class IngredientQuantityRequest {
     private List<IngredientQuantityDTO> ingredients;
 
-    // Getters and Setters
-    public List<IngredientQuantityDTO> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(List<IngredientQuantityDTO> ingredients) {
-        this.ingredients = ingredients;
-    }
 }
