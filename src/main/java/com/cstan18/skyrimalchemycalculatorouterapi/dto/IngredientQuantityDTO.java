5b7128cd29720ea0d6ad3c7908edcfb93f2a1ee5
Caleb Standridge <caleb.standridge@csuglobal.edu>
2024-05-30 08:59:55 -0500

    request to response working

--- src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/dto/IngredientQuantityDTO.java
+++ src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/dto/IngredientQuantityDTO.java
@@ -1,23 +1,11 @@
 package com.cstan18.skyrimalchemycalculatorouterapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class IngredientQuantityDTO {
     private Long ingredientId;
     private int quantity;
-
-    // Getters and Setters
-    public Long getIngredientId() {
-        return ingredientId;
-    }
-
-    public void setIngredientId(Long ingredientId) {
-        this.ingredientId = ingredientId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 }
