44d5c22b989b34548b4fc881460f8637ca455e6f
Caleb Standridge <caleb.standridge@csuglobal.edu>
2024-06-14 11:31:35 -0500

    add recipe logic

--- src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/model/PotionRecipe.java
+++ src/main/java/com/cstan18/skyrimalchemycalculatorouterapi/model/PotionRecipe.java
@@ -5,10 +5,13 @@ import lombok.Data;
 
 import java.util.List;
 
+
 @Data
 @AllArgsConstructor
 public class PotionRecipe {
-
-    private List<Ingredient> ingredientList;
+    private Long ingredient1Id;
+    private Long ingredient2Id;
+    private List<String> commonEffects;
+    private double value;
 
 }
